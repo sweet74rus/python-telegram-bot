193d1f359061f8996074762c47cceb2abad64bb1
Вячеслав Бородин <vikc003@mail.ru>
2022-03-08 19:03:16 +0500

    add holidays

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import telebot
+import re
+import requests
 
 bot = telebot.TeleBot('5133726676:AAFYkB4pOCGEaPKmXAQ3o3hFW4t8xDsHLj8')
-
+holiday_url = 'https://holidayapi.com/v1/holidays?pretty&key=c2475528-0e37-4d01-acaf-5192189a7adb&country=RU&language=RU'
+holiday_token = 'c2475528-0e37-4d01-acaf-5192189a7adb'
 
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
@@ -18,8 +21,21 @@ def start(message):
     if message.text == '/reg':
         bot.send_message(message.from_user.id, 'Как тебя зовут?')
         bot.register_next_step_handler(message, get_name)
+    elif message.text == '/data':
+        bot.send_message(message.from_user.id, 'Введи дату в таком формате: ДД-ММ-ГГГГ')
+        bot.register_next_step_handler(message, get_holiday_data)
     else:
-        bot.send_message(message.from_user.id, 'Напиши /reg')
+        bot.send_message(message.from_user.id, 'Напиши /reg - регистрация'
+                                               ' /data - узнать название праздника')
+
+
+def get_holiday_data(message):
+    a = re.match('([0-9]{1,2})-([0-9]{1,2})-([0-9]{4})', message.text)
+    try:
+        holiday_data = requests.get(holiday_url + f'&year=2021&month={a[2]}&day={a[1]}').json()
+        bot.send_message(message.from_user.id, holiday_data['holidays'][0]['name'])
+    except Exception:
+        bot.send_message(message.from_user.id, 'В этот день нет праздника:(')
 
 
 def get_name(message):
@@ -43,18 +59,29 @@ def get_age(message):
             age = int(message.text)
         except Exception:
             bot.send_message(message.from_user.id, 'Цифрами пожалуйста')
-        bot.send_message(message.from_user.id, f'Тебе {age} {get_true_age_word()}, '
+        word = get_true_age_word()
+        bot.send_message(message.from_user.id, f'Тебе {age} {word}, '
                                                f'тебя зовут {name}, '
                                                f' твоя фамилия {surname}')
 
 
 def get_true_age_word():
-    god = '1234'
+    god = '234'
     word = 'лет'
+    a = str(age)
 
-    if str(age)[-1] in god and str(age)[0] != '1':
-        word = 'год'
+    if len(a) > 1:
+        if str(age)[-1] in god and str(age)[0] != '1':
+            word = 'года'
+        elif str(age)[-1] == '1' and str(age)[0] != '1':
+            word = 'год'
+    else:
+        if str(age) == '1':
+            word = 'год'
+        elif str(age) in god:
+            word = 'года'
 
     return word
+
 
 bot.infinity_polling()
